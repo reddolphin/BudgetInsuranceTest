79b39c1233c0cf5502e02a5f78e81b0b1e20c6a0
reddolphin <alexchapman.ac2@gmail.com>
2015-07-03 22:39:25 +0100

    test

--- budgetinsurance/SitePages/AuthorPage.cs
+++ budgetinsurance/SitePages/AuthorPage.cs
@@ -5,7 +5,7 @@ namespace budgetinsurance.SitePages
 {
     public class AuthorPage
     {
-        [FindsBy(How = How.CssSelector, Using = "div.overview h1")]
+        [FindsBy(How = How.CssSelector, Using = "body > div.ng-scope > div:nth-child(2) > section.hero > div > div.small-12.large-10.medium-9.columns.pull-over > h1")]
         private IWebElement authorName;
         public string AuthorName
         {
