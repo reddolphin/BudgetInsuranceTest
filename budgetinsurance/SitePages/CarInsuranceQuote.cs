6e28dad9398cd530928d9b56d878bfebff0f340c
reddolphin <alexchapman.ac2@gmail.com>
2015-07-06 21:32:53 +0100

    add new page

---
+++ budgetinsurance/SitePages/CarInsuranceQuote.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace budgetinsurance.SitePages
+{
+    public class CarInsuranceQuote
+    {
+        //static string Url = "https://www.budgetinsurance.com/PrivateCar/Vehicle/Welcome.aspx";
+        static string Url = "https://www.budgetinsurance.com/PrivateCar/Vehicle/Welcome.aspx?ton_t=BUDPC&TDUID=&prdcls=PC&rqstyp=welcome&aid=";
+        
+        [FindsBy(How = How.CssSelector, Using = "#ctl00_CPH1_NewQuoteView_PostcodeView_TextBoxPostcode")]
+        private IWebElement postCodeTextBox;
+
+        public void PostCodeEnterText(string postcode)
+        {
+            postCodeTextBox.SendKeys(postcode);
+        }
+
+        public void Goto()
+        {
+            Browser.Goto(Url);
+        }
+        
+        public bool IsAt()
+        
+        {
+            IWebElement findTitle = Browser.Driver.FindElement(By.CssSelector("#HeaderContent .HeaderText .HeaderTextLarge"));
+            return findTitle.Text=="Welcome to your Car insurance quote";
+        }
+
+
+    }
+}
