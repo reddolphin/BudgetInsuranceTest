15a0c177acac8490b7f4be71152089cc073e95b4
reddolphin <alexchapman.ac2@gmail.com>
2015-07-06 21:46:05 +0100

    wet

---
+++ budgetinsurancetests/CarInsuranceQuoteTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+using budgetinsurance;
+using budgetinsurance.SitePages;
+
+namespace budgetinsurancetests
+{
+    [TestFixture]
+    class CarInsuranceQuoteTest
+    {
+        [Test]
+        public void Can_Go_To_CarInsuranceQuote()
+        {
+            //Pages.HomePage.Goto();
+            Pages.CarInsuranceQuote.Goto();
+            Assert.IsTrue(Pages.CarInsuranceQuote.IsAt());
+        }
+    }
+}
---
+++ budgetinsurancetests/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+using budgetinsurance;
+using budgetinsurance.SitePages;
+
+namespace budgetinsurancetests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void Can_Go_To_HomePage()
+        {
+            Pages.HomePage.Goto();
+            Assert.IsTrue(Pages.HomePage.IsAt());
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            Browser.Close();
+        }
+
+    }
+}
