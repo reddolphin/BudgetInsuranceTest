9ee5d626d122d43bcc30d86461d75a5c5b88e285
reddolphin <alexchapman.ac2@gmail.com>
2015-07-06 21:36:04 +0100

    jh

---
+++ budgetinsurance/Browser.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+//using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace budgetinsurance
+{
+    public static class Browser
+    {
+        //static IWebDriver webDriver = new FirefoxDriver();
+        static IWebDriver webDriver = new ChromeDriver();
+
+        public static string Title
+        {
+            get { return webDriver.Title; }
+        }
+
+        public static ISearchContext Driver
+        {
+            get { return webDriver; }
+        }
+
+        public static void Goto(string url)
+        {
+            webDriver.Url = url;
+        }
+
+        public static void Close()
+        {
+            webDriver.Close();
+        }
+    }
+}
